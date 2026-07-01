c8d407b93bd8774e03347db62cfa047998f1996b
IT26101947 <adeeshagirihagama123@gmail.com>
2026-07-01 10:37:50 +0530

    Add files via upload

---
+++ Q3/IT26101947Lab2Q3.java
@@ -0,0 +1,10 @@
+public class IT26101947Lab2Q3 {
+
+       public static void main (String [] args)
+	   {
+	       double sideA = 3;
+		   double sideB = 4;
+		   double hypotenus = Math.sqrt (sideA * sideA + sideB * sideB);
+		   System.out.println ("Length of the hypotenus="+hypotenus);
+	   }
+}
